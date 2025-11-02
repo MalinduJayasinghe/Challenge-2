fa17ff65f522c4c8df522f67c85a1fbb0f449660
MalinduJayasinghe <jayasinghemalindu3@gmail.com>
2025-11-02 12:23:10 +0530

    Add files via upload

---
+++ class Main01 {.java
@@ -0,0 +1,31 @@
+class Main01 {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {3, 0, -2, 7, 4, -5, 0};
+
+        int n = numbers.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (numbers[j] > numbers[j + 1]) {
+                    int temp = numbers[j];
+                    numbers[j] = numbers[j + 1];
+                    numbers[j + 1] = temp;
+                }
+            }
+        }
+
+        printArray(numbers);
+    }
+
+    public static void printArray(int[] arr) {
+        System.out.print("[");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+            if (i < arr.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println("]");
+    }
+}
